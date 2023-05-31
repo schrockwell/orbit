1676235d4117855949c8b01c7a674e699d226179
Rockwell Schrock <rockwell@schrock.me>
2023-05-31 10:35:06 -0400

    Trim trailing \n from ~G strings

--- lib/orbit/view.ex
+++ lib/orbit/view.ex
@@ -1,10 +1,22 @@
 defmodule Orbit.View do
   defmacro sigil_G({:<<>>, _, [binary]}, _modifier) do
-    compiled = EEx.compile_string(binary)
+    compiled =
+      binary
+      |> trim_trailing_newline()
+      |> EEx.compile_string()
 
     quote do
       {result, _binding} = Code.eval_quoted(unquote(compiled), assigns: var!(assigns))
       result
+    end
+  end
+
+  # Elixir herdocs (""") have a trailing "\n" which has will result in an undesired newline, so let's remove it
+  defp trim_trailing_newline(string) do
+    if String.ends_with?(string, "\n") do
+      String.slice(string, 0..-2)
+    else
+      string
     end
   end
 end
