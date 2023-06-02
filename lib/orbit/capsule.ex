b8475cd6b5b808fadefbe68c0c2e28a1b6e8983a
Rockwell Schrock <rockwell@schrock.me>
2023-06-02 08:28:37 -0400

    Add :ip, :cert, :cert_pem, :key, :key_pem options to Capsule

--- lib/orbit/capsule.ex
+++ lib/orbit/capsule.ex
@@ -5,12 +5,22 @@ defmodule Orbit.Capsule do
   ## Options
 
   ### Required
-  - `:certfile` - path to a TLS certificatefile in the PEM format
+
   - `:endpoint` - the `Orbit.Pipe` entry point that gets called on every request, typically an `Orbit.Router`
-  - `:keyfile` - path to a TLS private key file in the PEM format
+
+  - One of:
+    - `:certfile` - path to a TLS certificatefile in the PEM format
+    - `:cert` - a DER-encoded certificate binary
+    - `:cert_pem` - a PEM-encoded certificate binary
+
+  - One of:
+    - `:keyfile` - path to a TLS private key file in the PEM format
+    - `:key` - a DER-encoded private key binary
+    - `:key_pem` - a PEM-encoded private key binary
 
   ### Optional
 
+  - `:ip` - the IP to listen on; could be `:any`, `:loopback`, or an address string; defaults to `:any`
   - `:port` - the port to listen on; defaults to 1965
 
   ## Example Child Specification
@@ -31,23 +41,22 @@ defmodule Orbit.Capsule do
 
   @impl true
   def init(opts) do
-    port = opts[:port] || 1965
     endpoint = opts[:endpoint] || "the :endpoint option is required"
-    certfile = opts[:certfile] || "the :certfile option is required"
-    keyfile = opts[:keyfile] || "the :keyfile option is required"
+
+    port = opts[:port] || 1965
+    ip = parse_address!(opts[:ip] || :any)
 
     ti_opts = [
       port: port,
       handler_module: Orbit.Handler,
       handler_options: %{endpoint: endpoint},
       transport_module: ThousandIsland.Transports.SSL,
-      transport_options: [
-        certfile: certfile,
-        ip: :any,
-        keyfile: keyfile,
-        verify_fun: {&verify_peer/3, %{}},
-        verify: :verify_peer
-      ]
+      transport_options:
+        [
+          ip: ip,
+          verify_fun: {&verify_peer/3, %{}},
+          verify: :verify_peer
+        ] ++ cert_opts!(opts) ++ key_opts!(opts)
     ]
 
     children = [
@@ -56,6 +65,60 @@ defmodule Orbit.Capsule do
 
     Supervisor.init(children, strategy: :one_for_one)
   end
+
+  defp cert_opts!(opts) do
+    cond do
+      certfile = opts[:certfile] ->
+        [certfile: certfile]
+
+      cert = opts[:cert] ->
+        [cert: cert]
+
+      cert_pem = opts[:cert_pem] ->
+        [cert: decode_pem!(:Certificate, cert_pem)]
+
+      :else ->
+        raise "the certificate was not provided; specify one of: :cert, :cert_pem, :certfile"
+    end
+  end
+
+  defp key_opts!(opts) do
+    cond do
+      keyfile = opts[:keyfile] ->
+        [keyfile: keyfile]
+
+      key = opts[:key] ->
+        [key: {:PrivateKeyInfo, key}]
+
+      key_pem = opts[:key_pem] ->
+        [key: {:PrivateKeyInfo, decode_pem!(:PrivateKeyInfo, key_pem)}]
+
+      :else ->
+        raise "the private key was not provided; specify one of: :key, :key_pem, :keyfile"
+    end
+  end
+
+  defp decode_pem!(type, pem) when is_binary(pem) do
+    case :public_key.pem_decode(pem) do
+      [{^type, der, _}] -> der
+      _ -> raise "could not decode #{type} PEM"
+    end
+  end
+
+  defp parse_address!(:any), do: :any
+  defp parse_address!(:loopback), do: :loopback
+
+  defp parse_address!(ip) when is_binary(ip) do
+    ip
+    |> String.to_charlist()
+    |> :inet.parse_address()
+    |> case do
+      {:ok, ip} -> ip
+      _ -> raise "could not parse :ip option #{inspect(ip)}"
+    end
+  end
+
+  defp parse_address!(ip), do: raise("invalid :ip option #{inspect(ip)}")
 
   # https://stackoverflow.com/a/32198900
   defp verify_peer(cert, {:bad_cert, :selfsigned_peer}, state) do
